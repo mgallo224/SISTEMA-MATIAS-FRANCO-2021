3f13ed0eb1b92b0bb5fa4f24d1d7300344488d61
mgallo224 <mgallo224@gmail.com>
2022-02-08 17:11:36 -0300

    pdf actualizado

--- modulos/cventas.py
+++ modulos/cventas.py
@@ -1,7 +1,7 @@
-from ast import Str
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+from unicodedata import name
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import tkinter as tk
@@ -26,23 +26,23 @@ def cventas():
             precio2_label.after(2000, auto)  
 
 
-    def pdf(obtener_producto,obtener_cantidad,obtener_precio,fecha,obmes,obaño):
+    def pdf(obtener_producto,obtener_cantidad,obtener_precio,fecha,obmes,obaño,nombre):
         rand = random.randint(1,999)
-        cvas = canvas.Canvas("facturas/"+str(obtener_producto)+"_"+str(obmes)+"-"+str(obaño)+"___"+str(rand)+".pdf", pagesize=letter)
+        cvas = canvas.Canvas("facturas/"+str(nombre)+"_"+str(obmes)+"-"+str(obaño)+"___"+str(rand)+".pdf", pagesize=letter)
         cvas.setLineWidth(.3)
         cvas.setFont('Helvetica', 11)
         cvas.drawString(30,750,'Casa de comidas "Delivery"')
         cvas.drawString(30,735,fecha)
         cvas.drawString(500,750,"Cantidad:"+obtener_cantidad)
         cvas.line(480,747,580,747)
-        cvas.drawString(275,725,"Pedido:")
-        cvas.drawString(500,725,obtener_producto)
+        cvas.drawString(205,725,"Pedido:")
+        cvas.drawString(300,725,obtener_producto)
         cvas.line(378,723,580,723)
         cvas.drawString(30,703,'Precio:')
         cvas.line(120,700,580,700)
         cvas.drawString(120,703,"$"+str(obtener_precio))
         cvas.save() 
-        return messagebox.showinfo("Carga exitosa", "El pedido fue cargado exitosamente, se genero en la carpeta del programa la factura de nombre: "+str(obtener_producto)+"_"+str(obmes)+"-"+str(obaño)+"___"+str(rand)+".pdf")
+        return messagebox.showinfo("Carga exitosa", "El pedido fue cargado exitosamente, se genero en la carpeta del programa la factura de nombre: "+str(nombre)+"_"+str(obmes)+"-"+str(obaño)+"___"+str(rand)+".pdf")
         
     def añadir_a_la_base():
         db = "root.db"
@@ -59,19 +59,21 @@ def cventas():
         fecha = fechayhora()
         obmes = mes()
         obaño = año()
+        lista_productos = []
         if not obtener_producto0:
             return messagebox.showerror("Error", "Debe seleccionar un producto")
         elif obtener_cantidad.isnumeric() == False:
             return messagebox.showerror('Error', 'El precio y cantidad deben ser numericos')
         elif len(obtener_cantidad)<=0:
             return messagebox.showerror('Error', 'Valores no validos, ingrese nuevamente los valores')
+        elif len(obtener_nombreyapellidocliente)<=0 or obtener_nombreyapellidocliente.isnumeric() == True:
+            return messagebox.showerror('Error', 'El campo "Nombre" es invalido')
         elif int(obtener_cantidad)<=0:
             return messagebox.showerror('Error', 'El precio y cantidad deben mayores a 0')
         elif obtener_nombreyapellidocliente.isnumeric() == True:
             return messagebox.showerror("Error","El nombre no es valido")
         else:
-        
-            if parallevar == 1 and (len(obtener_direccioncliente)<=0 or len(obtener_nombreyapellidocliente)<=0):
+            if parallevar == 1 and (len(obtener_direccioncliente)<=0):
                     messagebox.showerror('Error', 'Si es para llevar debes indicar el nombre y dirección del cliente')
             else:
                 for obtener_producto0 in obtener_producto0:
@@ -83,7 +85,7 @@ def cventas():
                         a = "El siguiente producto no posee en stock la cantidad ingresada: "
                         b = datap[2]
                         error = a + b
-                        messagebox.showerror("ERROR AL CARGAR LA VENTA", error)
+                        return messagebox.showerror("Error al cargar pedido", error)
                     else:    
                         cursor.execute("SELECT precioventa from stock where producto=?", (data0[2],))
                         precio = cursor.fetchone()
@@ -101,9 +103,12 @@ def cventas():
                         vxunidad = veces[1]+int(obtener_cantidad)
                         cursor.execute("UPDATE stock SET veces=?, vxunidad=? where producto=?", (veces[0]+1,vxunidad, data0[2]))
                         conn.commit()
-                        pdf(data0[2],obtener_cantidad,obtener_precio,fecha,obmes,obaño)
-                cursor.close()    
-                return cantidad.delete(0,END),nombreyapellidocliente.delete(0,END),direccioncliente.delete(0,END),ventana_cventas.checkbox_value.set(0),producto.selection_clear(0, tk.END)
+                        lista_productos.append(data0[2])
+                        print(lista_productos)
+                        pdfproducto = "/".join(lista_productos)
+                pdf(pdfproducto,obtener_cantidad,obtener_precio,fecha,obmes,obaño,obtener_nombreyapellidocliente)
+            cursor.close()    
+            return cantidad.delete(0,END),nombreyapellidocliente.delete(0,END),direccioncliente.delete(0,END),ventana_cventas.checkbox_value.set(0),producto.selection_clear(0, tk.END)
 
     if __name__ == "modulos.cventas":
         ventana_cventas = tk.Toplevel()
@@ -145,11 +150,11 @@ def cventas():
         precio2_label.place(x=200, y=150)
         auto()
         # Crear caja de texto nombre cliente
-        nombreyapellidocliente_label = Label(ventana_cventas, text="Nombre y apellido del cliente (Opcional):")
+        nombreyapellidocliente_label = Label(ventana_cventas, text="A nombre de:")
         nombreyapellidocliente_label.place(x=20, y=200)
         nombreyapellidocliente = tk.Entry(ventana_cventas)
         # Posicionarla en la ventana.
-        nombreyapellidocliente.place(x=250, y=200)
+        nombreyapellidocliente.place(x=200, y=200)
         # Crear caja de texto direccion cliente
         direccioncliente_label = Label(ventana_cventas, text="Dirección del cliente (Opcional):")
         direccioncliente_label.place(x=20, y=250)
@@ -173,4 +178,3 @@ def cventas():
         ventana_cventas['bg'] = '#3cba54'
         ventana_cventas.mainloop()
     return False
-
